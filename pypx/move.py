4149731a61cd7a8e218a0a92e235af614f517e06
Matthias Greiner <Matthias.Greiner@stud.uni-heidelberg.de>
2018-08-20 13:59:41 +0200

    added support for dicom query-files and a local storescp instance

--- pypx/move.py
+++ pypx/move.py
@@ -4,6 +4,7 @@ import subprocess
 # PYPX modules
 from .base import Base
 
+
 class Move(Base):
     """docstring for Move."""
     def __init__(self, arg):
@@ -12,10 +13,20 @@ class Move(Base):
     def command(self, opt={}):
         command = '--move ' + opt['aet_listener']
         command += ' --timeout 5'
-        command += ' -k QueryRetrieveLevel=SERIES'
-        command += ' -k SeriesInstanceUID=' + opt['series_uid']
 
-        return self.executable + ' ' + command + ' ' + self.commandSuffix()
+        # Run a StoreSCP server with the move command to retrieve files locally
+        if opt.get('store_local', False):
+            command += ' --port ' + opt['storescp_port']
+
+        # Support for dicom query files
+        if opt.get('query_files', False):
+            query_files = ' ' + opt['query_files']
+        else:
+            query_files = ''
+            command += ' -k QueryRetrieveLevel=SERIES'
+            command += ' -k SeriesInstanceUID=' + opt['series_uid']
+
+        return self.executable + ' ' + command + ' ' + self.commandSuffix() + query_files
 
     def run(self, opt={}):
         response = subprocess.run(
